664792a91efb6f5c6d8bb969b71fd2ddfeacbe92
awu <softengineerwu@gmail.com>
2016-10-27 15:07:08 +0800

    创建项目

---
+++ DuskRadio/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.PlayerFramework;
+using Microsoft.PlayerFramework.Adaptive;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Streaming.Adaptive;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace DuskRadio
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Player player;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            player = new Player();
+        }
+
+        private async void play_Click(object sender, RoutedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    await player.Play("http://ngcdn002.cnr.cn/live/jjzs48/index.m3u8");
+                });
+        }
+    }
+}
---
+++ DuskRadio/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Streaming.Adaptive;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace DuskRadio
+{
+    /// <summary>
+    /// 播放器，封装了<see cref="MediaElement"/>的常用操作
+    /// </summary>
+    class Player
+    {
+        private double volumn;
+        /// <summary>
+        /// 获取、设置<see cref="MediaElement"/>的音量
+        /// </summary>
+        public double Volumn
+        {
+            get
+            {
+                return volumn;
+            }
+            set
+            {
+                if (value > 1)
+                {
+                    volumn = 1;
+                }else if(volumn < 0)
+                {
+                    volumn = 0;
+                }
+
+                SetVolumn();
+            }
+        }
+        private MediaElement mediaElement;
+        private AdaptiveMediaSourceCreationResult amsResult;
+        public Player()
+        {
+            mediaElement = new MediaElement();
+            mediaElement.Volume = 1;
+        }
+
+        /// <summary>
+        /// 设置MediaElement音量
+        /// </summary>
+        private void SetVolumn()
+        {
+            mediaElement.Volume = volumn;
+        }
+
+        /// <summary>
+        /// 播放为<see cref="MediaElement"/>指定的链接
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<bool> Play(string url)
+        {
+            if (amsResult == null)
+                amsResult = await AdaptiveMediaSource.CreateFromUriAsync(new Uri(url, UriKind.Absolute));
+            if (amsResult.Status == AdaptiveMediaSourceCreationStatus.Success)
+            {
+
+                AdaptiveMediaSource ams = amsResult.MediaSource;
+                mediaElement.SetMediaStreamSource(ams);
+                mediaElement.Play();
+                return true;
+            };
+            return false;
+        }
+
+        /// <summary>
+        /// 停止播放
+        /// </summary>
+        public void Stop()
+        {
+            if (mediaElement != null)
+                mediaElement.Stop();
+        }
+
+        /// <summary>
+        /// 暂停播放
+        /// </summary>
+        public void Pause()
+        {
+            if (mediaElement != null)
+                mediaElement.Pause();
+        }
+    }
+}
